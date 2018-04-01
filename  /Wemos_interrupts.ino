36b4149c5732e24db6a1fee0f2ca8d4340f637ba
ksali98 <kamsharaf@gmail.com>
2018-04-01 11:04:19 -0500

    Home WiFi
    added connected check

---  /Wemos_interrupts.ino
+++  /Wemos_interrupts.ino
@@ -21,7 +21,7 @@
 
 const int byte_number = 6;  // # of bytes per sesnor array reading
 const int sensor_group_readings = 10;  // # of readings we will group together
-const char* ssid     = "Alta Vista";
+const char* ssid     = "Mbrace_JSU";
 const char* password = "alialiali";
 const char* host = "mbrace.xyz";
 const int   port = 80;
@@ -45,18 +45,15 @@ void setup() {
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(100);
-    Serial.println(".");
   }
-  Serial.println("Connected");
-  
   // SD Card Setup
   SD.begin();
-  dataFile = SD.open("WiFi_T.txt", FILE_WRITE);  // Set file name to be created on SD card
+  dataFile = SD.open("WiFiA.txt", FILE_WRITE);  // Set file name to be created on SD card
   // This part writes experiemnt specific data to SD card, Uncomment only prior to upload.
   
-    dataFile.write("Experiment specific Data: \r\n");
-    dataFile.write("Date: 03/29/2018 \r\nLocation: GCRL \r\nCodeFile:Wemos_interrupts  \r\nDataFile: WiFiA.txt \r\n");
-    dataFile.write("Comments: First WiFi experiemnt sending data to MBRACE.xyz, data_collector, with MAC as filename.\r\n\r\n\r\n");
+    dataFile.write("Experiment specific Data:");
+    dataFile.write("Date: 03/29/2018 | Location: GCRL |CodeFile:Wemos_interrupts   | DataFile: WiFiA.txt ");
+    dataFile.write("Comments: First WiFi experiemnt sending data to MBRACE.xyz, data_collector, with MAC as filename");
     dataFile.flush();
 
   // I2C Setup
