11063fd782b7cc36388060aac0d69da43d918a6c
Unknown <kamsharaf@gmail.com>
2018-06-22 10:39:21 -0500

    Low Power Code.
    Nano, daily files on SD, 8 sensors, 4 sleep times (10Hz, 1Hz, 1min, 1hr)

--- Low_Power/Low_Power.ino
+++ Low_Power/Low_Power.ino
@@ -16,11 +16,11 @@
 int data_block = 0; //  10 Second data Blocks
 byte sensors[400]; // 
 File dataFile;
-const String file_prefix = String("xxx"); // *** EDIT *** // first part of SD file name
+const String file_prefix = String("yyy"); // *** EDIT *** // first part of SD file name
 int file_number = 0;
-int readings_in_file = 0;
+long readings_in_file = 0;
 int sleep_time = 1; // *** EDIT *** // Enter reading frequency value here: Only 1, 2, 3 or 4.
-int readings_per_file = 0;  // 10*60*60*24 = 864000 is the default
+long readings_per_file = 0;  // 10*60*60*24 = 864000 is the default
                                  // 1 = 10Hz, 2 = 1Hz, 3 =  1min, 4 = 1hr.
 void open_file();
 
@@ -33,25 +33,38 @@ void setup() {
   pinMode(10, OUTPUT);
   SD.begin(10);
   dataFile = SD.open(file_prefix + file_number + ".txt", FILE_WRITE);  // Set file name to be created on SD card
-  dataFile.write("Experiment specific Data: \r\n");
+  dataFile.write("Experiment specific Data: \r\n"); //  *** EDIT  ***
   dataFile.flush();
   set_readings_per_file();
 }
     
 void loop() {
-  for(int i = 0; i < data_block; i++) {  // For a total of 800 bytes;
+  for(int i = 0; i < data_block; i++) { 
     if(i % 8 == 0 && i != 0) {
-      // Sleep for selected time, after every 8 bytes
       sleep();
     }
-    readings_in_file++; // after reading sensors but before going to sleep, increment readings counter
+    readings_in_file++; // number of bytes in file..
     sensors[i] = map(analogRead(i % 8), 0, 1023, 0, 255);  // Read in A0 to A7 into sensor array as single bytes
   }
-  dataFile.write("$$"); // write $$ and 800 bytes every 10 Seconds
+  dataFile.write("$$"); // write $$ and data block
   dataFile.write(sensors, data_block);
   dataFile.flush();
-
   open_file();
+}
+
+// Functions start here. *****************************
+
+void open_file() {
+  Serial.print(readings_in_file);
+  Serial.print(" : ");
+  Serial.println(readings_per_file);
+  delay(100);
+  if(readings_in_file >= readings_per_file){
+    dataFile.close();
+    file_number++;
+    readings_in_file = 0; 
+    dataFile = SD.open(file_prefix + file_number + ".txt", FILE_WRITE);  // Set file name to be created on SD card
+  } 
 }
 
 void sleep() {
@@ -76,31 +89,22 @@ void sleep() {
   }
 }
 
-void open_file() {
-  if(readings_in_file >= readings_per_file){
-    dataFile.close();
-    file_number++;
-    readings_in_file = 0; 
-    dataFile = SD.open(file_prefix + file_number + ".txt", FILE_WRITE);  // Set file name to be created on SD card
-  } 
-}
-
 void set_readings_per_file(){
   switch (sleep_time) {
     case 1: // 10Hz  write every 400 bytes, 5 seconds
-      readings_per_file = 864000;  // 00
+      readings_per_file = 6912000 ;  // bytes per day
       data_block = 400;
       break;
     case 2: // 1Hz writes every 80 bytes, 10 seconds
-      readings_per_file = 86400;
+      readings_per_file = 691200;
       data_block = 80;
       break;
     case 3: // 1 minuet, writes every 80 bytes, 10 minutes
-      readings_per_file = 1440;
+      readings_per_file = 11520;
       data_block = 80;
       break;
     case 4: // 1 hour, writes every 8 bytes, 1 hour.
-      readings_per_file = 24;
+      readings_per_file = 192;
       data_block = 8;
       break;
   }
