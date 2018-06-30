4c8192018cd8173a089d5db349dc1ed8d6d1768b
Unknown <kamsharaf@gmail.com>
2018-06-30 09:38:42 -0500

    Sensor Loading
    WIP

--- SensorLoading-LP/SensorLoading-LP.ino
+++ SensorLoading-LP/SensorLoading-LP.ino
@@ -1,20 +1,15 @@
 //Code for sensor and magnet placement checks.
-
 //This code works. Nano reads data from 8 sensors @ 10Hz and 
 //display the selected sample data on the screen.
 
-//  NANO Low Power
-
 #include <LowPower.h>
 #include <avr/io.h>
-
 
 const int byte_number = 8;  // # of bytes per sesnor array reading
 byte sensor[8];
 int sensor_number;
 
 int get_mapped_analog(int analog_pin);
-
 
 void setup() {
   Serial.begin(115200); // higher speed 
@@ -25,17 +20,12 @@ void setup() {
   pinMode(10, OUTPUT);
 
   Serial.println("Enter the sensor number you want to load:  ");
-  Serial.println("Or enter a 9 to quit");
-    
-   
-    
+  Serial.println("Or enter a 9 to quit");  
 }
 
 void loop() {
-
   if(Serial.available()){
     sensor_number = Serial.parseInt();
-
     if(sensor_number >= 0 && sensor_number < 8)
     {
       Serial.print("Changing to ");
@@ -45,31 +35,24 @@ void loop() {
       Serial.println("Off");
   }
   
-  
-   //get_sensor_number();
-   for(int i = 0; i<8 ; i++){
-     sensor[i] = get_mapped_analog(i);
-   }
-
-      if(sensor_number >= 0 && sensor_number < 8)
-      {
-          Serial.print("sensor number ");
-          Serial.print(sensor_number);
-          Serial.print(":  ");
-          Serial.println(sensor[sensor_number]);
-          delay(30);
-       }
-      
-      LowPower.idle(SLEEP_60MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_ON, TWI_OFF);
-      LowPower.idle(SLEEP_30MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_ON, TWI_OFF);
-       //LowPower.powerDown(SLEEP_60MS, ADC_OFF, BOD_OFF);
-       //LowPower.powerDown(SLEEP_30MS, ADC_OFF, BOD_OFF);      
- }
-
+  for(int i = 0; i<8 ; i++){
+  sensor[i] = get_mapped_analog(i);
+  }
+    if(sensor_number >= 0 && sensor_number < 8){
+      Serial.print("sensor number ");
+      Serial.print(sensor_number);
+      Serial.print(":  ");
+      Serial.println(sensor[sensor_number]);
+      delay(30);
+     }      
+    LowPower.idle(SLEEP_60MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_ON, TWI_OFF);
+    LowPower.idle(SLEEP_30MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_ON, TWI_OFF);
+     //LowPower.powerDown(SLEEP_60MS, ADC_OFF, BOD_OFF);
+     //LowPower.powerDown(SLEEP_30MS, ADC_OFF, BOD_OFF);      
+}
 
 int get_mapped_analog(int analog_pin){
       int sensor_value;
       sensor_value = map(analogRead(analog_pin), 0, 1023, 0, 255);
       return sensor_value;
     }
-
---
+++ sensorLoading-Wemos/sensorLoading-Wemos.ino
@@ -0,0 +1,47 @@
+//Code for sensor and magnet placement checks.
+
+//This code works. Wemos reads data from 6 sensors @ 10Hz and 
+//display the selected sample data on the screen.
+
+//  NANO on top......
+
+
+#include <ESP8266WiFi.h>
+#include <Wire.h>
+
+const int byte_number = 6;  // # of bytes per sesnor array reading
+byte sensor_value[6];
+
+void setup() {
+  Serial.begin(9600); // higher speed 
+  Wire.begin(); //   I2C Setup
+  Wire.setClockStretchLimit(40000);  // In µs for Wemos D1 and Nano
+  delay(100);  // Short delay, wait for the Mate to send back CMD
+}
+
+void loop() {
+    Serial.println("Enter the sensor number you want to load:  ");
+    Serial.println("Or enter a 9 to quit");
+    
+    while(!Serial.available()){
+      //Wait for input
+    }
+    int sensor_number = Serial.parseInt();
+    Serial.println("got the number");
+    while(sensor_number != 9){  
+       Wire.requestFrom(1, byte_number);
+        while (Wire.available()) {
+          for (int i = 0; i < 6; i++) {
+            sensor_value[i] = Wire.read();
+          }
+          Serial.print("sensor number ");
+          Serial.print(sensor_number);
+          Serial.print(":  ");
+          Serial.println(sensor_value[sensor_number]);
+         }
+         if(Serial.available()){
+          sensor_number = Serial.parseInt();
+         }
+    }
+
+}
