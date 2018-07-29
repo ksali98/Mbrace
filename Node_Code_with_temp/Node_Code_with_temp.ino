ef031a67b2cbfd82d3f157545deea46ae041cffa
Unknown <kamsharaf@gmail.com>
2018-07-28 23:20:35 -0500

    Node Code with temp
    7 bytes, temp*5, one byte. tested

--- Node_Code_with_temp/Node_Code_with_temp.ino
+++ Node_Code_with_temp/Node_Code_with_temp.ino
@@ -1,27 +1,30 @@
-// Slave Nano with 6 sernsors. Sends 6 bytes of data plus one byte of temp with wire requests.
-// uses 5V I2C bus. Water temp, therefore temp*8
-// Kamal S. Ali, 07/25/2018
+// Slave Nano with 6 sernsors. Sends 6 bytes of data plus one byte of temperature with wire requests.
+// uses 5V I2C bus. Water temp is X5. 10K Pullup on temp sensor.
+// Kamal S. Ali, 07/28/2018
 
 #include <Wire.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
- 
 #define byte_number 7
 #define ONE_WIRE_BUS 2 //  edit this number
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+int get_mapped_temperature();
  
 byte sensor[byte_number]; 
+byte temp;
 
 void setup() {
   Wire.begin(1);                // join i2c bus with address 1
   Wire.onRequest(requestEvent); 
-  
+//  Serial.begin(9600);
   sensors.begin();
 }
 
 void loop() {
-  // wait for I2C request....
+//  temp = get_mapped_temperature();
+//  Serial.println(temp);
+//  delay (200);
 }
 
 void requestEvent() {
@@ -41,5 +44,5 @@ int get_mapped_analog(int analog_pin) {
 
 int get_mapped_temperature() {
   sensors.requestTemperatures(); // Send the command to get temperatures 
-  return map((int)(sensors.getTempCByIndex(0)*8), 0, 255, 0, 255);
+  return (byte)(sensors.getTempCByIndex(0)*5);
 }
