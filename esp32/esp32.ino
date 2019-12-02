1e7eef12895124b6468a09010ed0a455120b7e24
Curtis <Jamesaddy789@gmail.com>
2019-12-02 14:21:17 -0600

    Added a timestamp for when the device starts. Refactored some repeated code into funtions.

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -10,6 +10,7 @@
 #include <SD.h>
 #include <SPI.h>
 #include <string.h>
+
 ///////////Wi-Fi////////////////////////
 #include "esp_wpa2.h" //wpa2 library for connections to Enterprise networks
 #include <WiFi.h> //Wifi library
@@ -22,9 +23,11 @@ const char * wifi_username = "";
 const char * wifi_password =  "";
 
 WiFiUDP ntp_udp;
-NTPClient timeClient(ntp_udp);
-
+NTPClient time_client(ntp_udp);
 ////////////end of Wi-Fi////////////////
+
+//Array for holding startup time from NTP server
+byte startup_time[7];
 
 const int byte_number = 6;  // # of bytes per sesnor array reading
 const int sensor_group_readings = 10;  // # of readings we will group together before writing to sd card'
@@ -53,17 +56,16 @@ void setup() {
   Serial.begin(115200); // higher speed
   Serial.println("start");
 
-  //WIFI Setup
-  while (WiFi.status() != WL_CONNECTED) {
-    if(wifi_username == "")
-    {
-       WIFI_Connect_Normal();
-    }
-    else
-    {
-       WIFI_Connect_Enterprise(wifi_username, wifi_password); 
-    }           
+  //WIFI Setup  
+  if(wifi_username == "")
+  {
+     WIFI_Connect_Normal();
   }
+  else
+  {
+     WIFI_Connect_Enterprise(wifi_username, wifi_password); 
+  }           
+
   delay(100);
   
   // SD Card Setup
@@ -71,7 +73,7 @@ void setup() {
   dataFile = SD.open(file_prefix + String(day_counter) + ".DAT", FILE_WRITE);  // Set file name to be created on SD card
   //dataFile.write("Experiment Data: \r\n");
   //dataFile.write("Date: MM/dd/YYYY -- HH:mm \r\nLocation: GCRL \r\nCodeFile:esp32  \r\nDataFile: GCRL-nn \r\n");  // ******EDIT******
-  //dataFile.write("Comments: Daily files, @@,millils(),## followed by 60 bytes per second..\r\n\r\n\r\n");  // ******EDIT******
+  //dataFile.write("Comments: Daily files, @@,millis(),## followed by 60 bytes per second..\r\n\r\n\r\n");  // ******EDIT******
   dataFile.flush();
   file_start_time = millis() - get_time_in_seconds() * 1000;
 
@@ -84,10 +86,21 @@ void setup() {
   timerAttachInterrupt(timer, &onTimerISR, true);
   timerAlarmWrite(timer, 100000, true);
   timerAlarmEnable(timer);
+
+  //Initialize the NTPClient object to get time
+  time_client.begin();
+  // Set offset time in seconds to adjust for your timezone, for example:
+  // GMT 0 = 0 
+  // GMT +1 = 3600
+  // GMT -1 = -3600
+  // GMT -6 = -21600 (This is CST in the U.S.)
+  time_client.setTimeOffset(-21600);
+  Set_Startup_Time();
+  Record_Startup_Time();
 }
 
 void loop() {
-  while (WiFi.status() != WL_CONNECTED) {
+  if(WiFi.status() != WL_CONNECTED) {
     if(wifi_username == "")
     {
        WIFI_Connect_Normal();
@@ -95,13 +108,16 @@ void loop() {
     else
     {
        WIFI_Connect_Enterprise(wifi_username, wifi_password); 
-    }           
+    }   
+    Set_Startup_Time();   
+    Record_Startup_Time();     
   }
   
   if (payload_length == byte_number * sensor_group_readings + 8) {
     open_file();
     dataFile.write(sensor_payload, payload_length);
     dataFile.flush();   
+    //base64_encode will modify the current sensor_payload
     base64_encode((char*)output_payload, (char*)sensor_payload, payload_length);
     send_payload(output_payload);
     payload_length = 0;
@@ -139,7 +155,7 @@ void IRAM_ATTR onTimerISR(){
   interrupted = true;
 }
 
-//Send WiFi data
+//Send data to server
 void send_payload(byte *payload) {
   HTTPClient http;
   String data_string = String((char*)payload);
@@ -302,8 +318,10 @@ inline unsigned char b64_lookup(char c) {
 
 void WIFI_Connect_Normal()
 {
-  WiFi.begin(ssid, wifi_password);
-  Wait_For_Connection()
+  Serial.print("Connecting to network: ");
+  Serial.println(ssid);
+  WiFi.disconnect(true);  //disconnect form wifi to set new wifi connection
+  Wait_For_Connection();
 }
 
 void WIFI_Connect_Enterprise(const char * username, const char * password) {
@@ -316,28 +334,66 @@ void WIFI_Connect_Enterprise(const char * username, const char * password) {
   esp_wifi_sta_wpa2_ent_set_password((uint8_t *)password, strlen(password)); //provide password
   esp_wpa2_config_t config = WPA2_CONFIG_INIT_DEFAULT(); //set config settings to default
   esp_wifi_sta_wpa2_ent_enable(&config); //set config settings to enable function
-  WiFi.begin(ssid); //connect to wifi
-  Wait_For_Connection()
+  Wait_For_Connection();
 }
 
 void Wait_For_Connection()
 {
-   int counter = 0;
-   while (WiFi.status() != WL_CONNECTED){
-      if(counter >= 20)
-      {
-        Serial.println("No connection in 10 seconds. Retrying connection");
-        WiFi.begin(ssid, wifi_password);
-        counter = 0;
-      }
-      delay(500);
-      Serial.print(".");
-      counter += 1;
-   }
-  
-    Serial.println("connected to " + String(ssid));
-    Serial.println();
-    Serial.println("WiFi connected");
-    Serial.println("IP address set: ");
-    Serial.println(WiFi.localIP());
+  WiFi.begin(ssid, wifi_password);
+  int counter = 0;
+  while(WiFi.status() != WL_CONNECTED){
+    if(counter >= 20)
+    {
+      Serial.println("No connection in 10 seconds. Retrying connection");
+      WiFi.begin(ssid, wifi_password);
+      counter = 0;
+    }
+    delay(500);
+    Serial.print(".");
+    counter += 1;
+  } 
+  Serial.println("connected to " + String(ssid));
+  Serial.println();
+  Serial.println("WiFi connected");
+  Serial.println("IP address set: ");
+  Serial.println(WiFi.localIP());
+}
+
+void Set_Startup_Time()
+{
+  while(!time_client.update()) {
+    time_client.forceUpdate();
+  }
+
+  // The formatted date comes with the following format:
+  // 2018-05-28T16:00:13Z
+  // We need to extract date and time
+  String formatted_date = time_client.getFormattedDate();
+
+  // Find T
+  int T_index = formatted_date.indexOf("T");
+  // Extract time
+  String time_stamp = formatted_date.substring(T_index+1, formatted_date.length()-1);
+  //HH:mm:ss
+  //01234567
+
+  //Set the byte array to {{HHmmss}}
+  startup_time[0] ='{';
+  startup_time[1] ='{';
+  startup_time[2] = time_stamp.substring(0,2).toInt();
+  startup_time[3] = time_stamp.substring(3,5).toInt();
+  startup_time[4] = time_stamp.substring(6,8).toInt();
+  startup_time[5] ='}';
+  startup_time[6] ='}'; 
+}
+
+void Record_Startup_Time()
+{
+  open_file();
+  dataFile.write(startup_time, 7);
+  dataFile.flush();   
+  byte startup_time_output[7 * 4 / 3];
+  //base64_encode will modify the current payload
+  base64_encode((char*)startup_time_output, (char*)startup_time, 7);
+  send_payload(startup_time_output);
 }
